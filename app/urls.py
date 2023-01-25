9cf1aa43ce93d8464aa16758d3853a4ec967c784
Hugo Pino <swenkkros@gmail.com>
2023-01-26 00:02:21 +0100

    Refactored HTML structure to create reusable base template

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.home, name='home'),
     path('services/', views.services, name='services'),
     path('shop/', views.shop, name='shop'),
-    path('blog/', views.blog, name='shop'),
+    path('blog/', views.blog, name='blog'),
     path('contact/', views.contact, name='contact'),
 ]
