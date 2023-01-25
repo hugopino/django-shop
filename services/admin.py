7c32b6d96fb5e11334258c353a7ea553d26112ad
Hugo Pino <swenkkros@gmail.com>
2023-01-26 00:41:59 +0100

    Created new app 'Services'

---
+++ services/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Service 
+
+
+class AdminService(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+admin.site.register(Service, AdminService)
